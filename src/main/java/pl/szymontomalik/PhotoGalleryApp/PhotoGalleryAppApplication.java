fd4feb4fab4c208cfd8965481e38b90905eb8019
SzymonTomalik <stomalik@gmail.com>
2021-02-05 23:40:17 +0100

    Initial commit

---
+++ src/main/java/pl/szymontomalik/PhotoGalleryApp/PhotoGalleryAppApplication.java
@@ -0,0 +1,13 @@
+package pl.szymontomalik.PhotoGalleryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoGalleryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoGalleryAppApplication.class, args);
+	}
+
+}
