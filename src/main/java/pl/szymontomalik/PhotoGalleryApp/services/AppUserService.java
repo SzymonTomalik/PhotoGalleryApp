146886e20e814d5198ebe424f28768c273e64765
SzymonTomalik <stomalik@gmail.com>
2021-02-07 10:48:20 +0100

    registration&login

---
+++ src/main/java/pl/szymontomalik/PhotoGalleryApp/services/AppUserService.java
@@ -0,0 +1,1 @@
+package pl.szymontomalik.PhotoGalleryApp.services;import pl.szymontomalik.PhotoGalleryApp.entities.AppUser;public interface AppUserService {    AppUser findByEmail(String email);    void addUser (AppUser appUser);}
