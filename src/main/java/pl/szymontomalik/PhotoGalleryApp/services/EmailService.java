f5dd921ffd20a129e5b676c7e65aab831ff34d28
SzymonTomalik <stomalik@gmail.com>
2021-02-08 00:08:22 +0100

    create albums and send password functionality

---
+++ src/main/java/pl/szymontomalik/PhotoGalleryApp/services/EmailService.java
@@ -0,0 +1,1 @@
+package pl.szymontomalik.PhotoGalleryApp.services;public interface EmailService {    void sendSimpleMessage(String to,                           String subject,                           String text);}
